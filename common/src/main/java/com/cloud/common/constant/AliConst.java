4a98c5a7f81857a8db746ca108633e45ef37db3b
jyan <jyan_8001@yeah.net>
2021-02-20 13:33:46 +0800

    fixbug(jyan)

--- common/src/main/java/com/cloud/common/constant/AliConst.java
+++ common/src/main/java/com/cloud/common/constant/AliConst.java
@@ -1,11 +1,11 @@
 package com.cloud.common.constant;
 
 public class AliConst {
-    public static final String smsKeyId = "LTAI0jcWBwaVQaAW";
-    public static final String smsSecret = "4c23yTh4kxxSXCSdLK5qIK8AO8WZmX";
-    public static final String ossKeyId = "LTAICsphAfdoRSNF";
-    public static final String ossSecret = "7lbOs6st3vseJK3WptsHWY5OzoAOXf";
-    public static final String ossUrl = "https://suemei.oss-cn-hangzhou.aliyuncs.com";
-    public static final String ossEndpoint = "https://oss-cn-hangzhou.aliyuncs.com";
-    public static final String ossBucketName = "suemei";
+    public static final String smsKeyId = "*";
+    public static final String smsSecret = "*";
+    public static final String ossKeyId = "*";
+    public static final String ossSecret = "*";
+    public static final String ossUrl = "*";
+    public static final String ossEndpoint = "*";
+    public static final String ossBucketName = "*";
 }
